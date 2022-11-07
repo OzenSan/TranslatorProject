daec6ec0758206feab7707c229a0d33ab4c73654
OzenSan <adamsznapi@gmail.com>
2022-11-07 09:27:45 +0100

    Init

---
+++ src/main/java/com/example/translatorproject/TranslatorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.translatorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslatorProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/translatorproject/TranslatorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.translatorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
